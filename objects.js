47e92a15fb640547fe52783c4c5d7d8297d452f9
Timothy Tan <timtan93@gmail.com>
2018-11-24 23:58:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistName: "songTITLE"}
 
-function removePlaylist (playlist, artistName, songTitle){
+function updatePlaylist (playlist, artistName, songTitle){
   playlist.artistName = 'songTitle'
 }
