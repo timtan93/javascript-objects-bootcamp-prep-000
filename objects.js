14335ae3691a4dc3dc8ac762a056f02afb814ea3
Timothy Tan <timtan93@gmail.com>
2018-11-25 00:02:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist (playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist (playlist, artistName){
-delete playlist[artistname]
+delete playlist[artistName]
 return playlist 
 }
